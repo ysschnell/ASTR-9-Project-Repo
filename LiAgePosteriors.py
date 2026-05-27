a3bbe2977293461271f76c3559feaf18f2673799
Will <willmccullough@protonmail.com>
2026-05-27 09:31:41 -0700

    Update LiAgePosteriors.py

--- LiAgePosteriors.py
+++ LiAgePosteriors.py
@@ -1,15 +1,8 @@
-#%%
-from glob import glob
-star_name = "HIP56960"
-
-for filepath in glob(f'D:/*/*/*/*{star_name}*_spectrum.fits'):
-    print(filepath)
-#%%
 import pandas as pd
 
-x = 56960
+x = 56960 #idenifier goes here
 
-HIPc = pd.read_csv('C:/Users/b17de/Downloads/asu.tsv', sep=';', comment='#')
+HIPc = pd.read_csv('C:/Users/b17de/Downloads/asu.tsv', sep=';', comment='#') #swap this to your directory
 
 bmv = HIPc.at[x,"B-V"]
 HIPi = HIPc.at[x, "HIP"]
@@ -18,17 +11,14 @@ print(bmv, HIPi)
 
 import sys
 from pathlib import Path
-baffles_dir = Path("C:/Users/b17de/BAFFLES")
+baffles_dir = Path("C:/Users/b17de/BAFFLES") #swap this to your directory
 sys.path.insert(0, str(baffles_dir))
 import baffles
 
 import os
 currentdir=os.getcwd()
 os.chdir(str(baffles_dir))
-posterior = baffles.baffles_age(bv=float(bmv),li=229.3, 
+posterior = baffles.baffles_age(bv=float(bmv),li=229.3, #change as needed 
                                 li_err=2, fileName=star_name+"Age_Posterior", savePlots=False)
 os.chdir(currentdir)
-# %%
 import pandas as pd
-HIPc.at(x,"HIP")
-# %%
